ab858bac12760a1cb6c600fbd3cb393c354ea1b1
Vipin Kumar <vipinthakur3881@gmail.com>
2022-03-19 23:56:59 +0530

    added template files

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,11 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from mysite.blog import views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('', include('blog.urls'))
 ]
